56031b1e49264b2442b87a36345ff808f1fb1abc
吕海涛 <git@lvht.net>
2015-07-15 22:44:43 +0800

    添加 Blade 使用示例

---
+++ README.md
@@ -0,0 +1,4 @@
+Laravel Blade 学习
+==================
+
+单独使用 Larave Blade 模板系统示例。
---
+++ views/vim.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+<p>以思考的速度编辑文本！！！</p>
+<ul>
+@foreach($editors as $editor)
+<li>{{ $editor }}</li>
+@endforeach
+</ul>
+@endsection
+
+@if($a==1)
+<p>@{{呵呵}}</p>
+@endif
